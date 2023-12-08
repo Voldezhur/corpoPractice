3d4a4934b97296297a34cbc42a66ea44dedfb1ee
Voldezhur <voldezhur@gmail.com>
2023-12-08 10:55:11 +0300

    started 6th practice

---
+++ corpoPractice6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Microsoft.VisualBasic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
